d47c88d2095711eae2eb3aac4c431895fb89b410
P V A V RAMANA VENKATESH <63536144+pasupuletivenkatesh1900@users.noreply.github.com>
2022-10-27 18:19:42 +0530

    started the brower

---
+++ src/reliance/Automate.java
@@ -0,0 +1,17 @@
+package reliance;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automate {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\d\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+		Thread.sleep(10000);
+		driver.close();
+	}
+
+}
